1085e9bbf140e8f2eccffa0c82701acde6667743
Tom Offermann <tom@offermann.name>
2022-12-12 08:13:05 +0100

    cleaned up/commented 11

--- 11.js
+++ 11.js
@@ -1,6 +1,7 @@
 let lines = require("fs").readFileSync("./files/11.txt", "utf-8").split("\n\n");
 let monkeys = [];
 
+// Monkey class
 class Monkey {
   items = [];
   count = 0;
@@ -9,6 +10,7 @@ class Monkey {
   div = 0;
   op = "";
   v = "";
+  // Init a monkey
   constructor(items, to1, to2, operation, div) {
     this.items = items;
     this.to1 = to1;
@@ -17,27 +19,31 @@ class Monkey {
     this.v = operation.split("=")[1].split(" ")[3];
     this.div = div;
   }
+  // Throw an item to monkey (v)
   throwTo(v) {
     this.items.push(v);
   }
-  round() {
-    while(this.items.length > 0) {
+  round(task1) {
+    while (this.items.length > 0) {
       this.count++;
       let old = this.items.shift();
       let vNum = 0;
-      if(this.v === "old") {
+      if (this.v === "old") {
         vNum = old;
       } else {
         vNum = parseInt(this.v);
       }
       let n = 0;
-      if(this.op == "*") {
+      if (this.op == "*") {
         n = old * vNum;
       } else {
         n = old + vNum;
       }
-      //n = Math.floor(n / 3);
-      if(n % this.div == 0) {
+      if (task1) {
+        n = Math.floor(n / 3);
+      }
+      // Throw-to conditions:
+      if (n % this.div == 0) {
         monkeys[this.to1].throwTo(n);
       } else {
         monkeys[this.to2].throwTo(n);
@@ -46,32 +52,46 @@ class Monkey {
   }
 }
 
+// parseing a monkey from one "chunk" of monkey data
 let parseMonkey = (monkey) => {
   let list = monkey.split("\n");
-  let items = list[1].split(":")[1].split(",").map(e => parseInt(e));
+  let items = list[1]
+    .split(":")[1]
+    .split(",")
+    .map((e) => parseInt(e));
   let operation = list[2].split(":")[1];
   let div = parseInt(list[3].split(" ")[list[3].split(" ").length - 1]);
   let to1 = parseInt(list[4].split(" ")[list[4].split(" ").length - 1]);
   let to2 = parseInt(list[5].split(" ")[list[5].split(" ").length - 1]);
   return new Monkey(items, to1, to2, operation, div);
+};
+
+monkeys = lines.map((e) => parseMonkey(e));
+
+for(let i = 0; i < 20; i++) {
+  for(let j = 0; j < monkeys.length; j++) {
+    monkeys[j].round(true);
+  }
 }
 
-monkeys = lines.map(e => parseMonkey(e));
+// Find the two most active monkeys
+monkeys.sort((a, b) => b.count - a.count);
+console.log("Task 1: ", monkeys[0].count * monkeys[1].count);
 
-for(let i = 0; i < 10000; i++) {
-  //console.log(monkeys.map(e => e.items))
+// Re-parse the monkey to our default
+monkeys = lines.map((e) => parseMonkey(e));
+
+for (let i = 0; i < 10000; i++) {
   let mod = 1;
-  for(let j = 0; j < monkeys.length; j++) {
+  for (let j = 0; j < monkeys.length; j++) {
     mod *= monkeys[j].div;
-    monkeys[j].round();
-    if(j == 3 && i == 19) {
-      console.log(monkeys);
-    } 
+    monkeys[j].round(false);
   }
-  for(let j = 0; j < monkeys.length; j++) {
-    monkeys[j].items = monkeys[j].items.map(e => e % mod);
+  for (let j = 0; j < monkeys.length; j++) {
+    monkeys[j].items = monkeys[j].items.map((e) => e % mod);
   }
 }
 
-monkeys.sort((a,b) => b.count - a.count );
-console.log(monkeys[0].count * monkeys[1].count);
+// Find the two most active monkeys
+monkeys.sort((a, b) => b.count - a.count);
+console.log("Task 2: ", monkeys[0].count * monkeys[1].count);
